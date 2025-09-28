3faf02b48049e3cb6478b0f842b0e2ccef60fc11
sanjog <sanjogpariyar17@gmail.com>
2025-09-28 15:51:51 +0545

    ...112

--- tests/database.py
+++ tests/database.py
@@ -8,7 +8,7 @@ import pytest
 from app.database import get_db, Base
 
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test"
+SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
